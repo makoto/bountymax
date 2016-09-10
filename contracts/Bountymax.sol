cfedcfc9e2121c72ec1ee90a51f01661d782fb1e
Andrew Jones <ascjones@gmail.com>
2016-09-10 18:02:16 +0100

    Add license to Bountymax contract

--- contracts/Bountymax.sol
+++ contracts/Bountymax.sol
@@ -1,3 +1,20 @@
+// Copyright (c) 2016 Andrew Jones
+//
+// This file is part of BOUNTYMAX.
+//
+// BOUNTYMAX is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 contract Bountymax {
   struct Bounty {
     string name;
@@ -41,7 +58,7 @@ contract Bountymax {
       // use withdrawal pattern to avoid reentrancy bug
       uint reward = bounties[exp.target].reward;
       bounties[exp.target].reward = 0;
-      
+
       if (exp.hunter.send(reward)) {
         BountyClaimed({target: exp.target, hunter: exp.hunter, amount: reward});
         // todo: remove exploit?
