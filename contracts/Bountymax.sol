e162e4a1f3dc74da015684c2a019214ed7dff2d8
Andrew Jones <ascjones@gmail.com>
2016-09-10 19:08:14 +0100

    Store exploit requests by oraclize request id

--- contracts/Bountymax.sol
+++ contracts/Bountymax.sol
@@ -23,13 +23,14 @@ contract Bountymax {
     uint reward;
   }
 
-  struct Exploit {
+  struct ExploitRequest {
+    address exploit;
     address target;
     address hunter;
   }
 
   mapping (address => Bounty) public bounties;
-  mapping (address => Exploit) public exploits;
+  mapping (bytes32 => ExploitRequest) public exploitRequests;
 
   function register(string name, address target, address invariant) public {
     // todo: avoid overwriting existing bounty?
@@ -40,40 +41,36 @@ contract Bountymax {
   function exploit(address target, address bounty, address exploit) public {
       // todo: check if bounty already claimed?
       // for now just simulate that it succeeded
+      bytes32 requestId = 0; // todo: query oraclize here
 
       // so we can pay the hunter if it succeeds
-      exploits[exploit] = Exploit({target: target, hunter: msg.sender});
+      exploitRequests[requestId] = ExploitRequest({exploit: exploit, target: target, hunter: msg.sender});
 
       // call the callback directly for now
       // todo: remove this call directly to callback with call to oraclize
-      __callback(msg.sender, "true");
+      __callback(requestId, "true");
   }
 
-  function __callback(address exploit, string result) {
+  function __callback(bytes32 requestId, string result) {
     // todo: add this back once we add in oraclise API contract
     /*if (msg.sender != oraclize_cbAddress()) throw;*/
-    Exploit exp = exploits[exploit];
+    ExploitRequest exploit = exploitRequests[requestId];
+    // todo: remove exploit request?
     if (sha3(result) == sha3("true")) {
 
       // use withdrawal pattern to avoid reentrancy bug
-      uint reward = bounties[exp.target].reward;
-      bounties[exp.target].reward = 0;
+      uint reward = bounties[exploit.target].reward;
+      bounties[exploit.target].reward = 0;
 
-      if (exp.hunter.send(reward)) {
-        BountyClaimed({target: exp.target, hunter: exp.hunter, amount: reward});
-        // todo: remove exploit?
+      if (exploit.hunter.send(reward)) {
+        BountyClaimed({target: exploit.target, hunter: exploit.hunter, amount: reward});
       } else {
-        bounties[exp.target].reward = reward;
+        bounties[exploit.target].reward = reward;
       }
     } else {
-      ExploitFailed({exploit: exploit, target: exp.target});
+      ExploitFailed({exploit: exploit.exploit, target: exploit.target});
     }
   }
-
-  event ExploitFailed (
-    address exploit,
-    address target
-  );
 
   event BountyRegistered (
     string name,
@@ -86,5 +83,10 @@ contract Bountymax {
     address target,
     address hunter,
     uint amount
+  );
+
+  event ExploitFailed (
+    address exploit,
+    address target
   );
 }
