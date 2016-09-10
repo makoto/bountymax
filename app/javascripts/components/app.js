566b5aee90ec6e38891a6609b7be4bd8c76d9a48
Makoto Inoue <inouemak@googlemail.com>
2016-09-10 14:25:20 +0100

    Add UI skin

--- app/javascripts/components/app.js
+++ app/javascripts/components/app.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 import Avatar from 'material-ui/Avatar';
 import AppBar from 'material-ui/AppBar';
 import {List, ListItem} from 'material-ui/List';
 import Paper from 'material-ui/Paper';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
 
 const App = (props) => (
   <MuiThemeProvider muiTheme={getMuiTheme()}>
     <div>
       <AppBar titleStyle={{textAlign:'center', fontSize:'xx-large', fontFamily:'sans-serif'}} style={{backgroundColor:"#46ae32"}}
         title={
-          <span>TITLE<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - Some one liner -</span></span>
+          <span>BountyMax<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - The Mad Max of Cyber Bounty  -</span></span>
         }
         iconElementLeft={<Avatar src="https://15254b2dcaab7f5478ab-24461f391e20b7336331d5789078af53.ssl.cf1.rackcdn.com/ethereum.vanillaforums.com/favicon_85d47ba50743e3c3.ico" size={50} backgroundColor="white" />}
         iconElementRight={
@@ -24,18 +31,43 @@ const App = (props) => (
       />
       <div className='container'>
         <Paper zDepth={1} style={{height:'500px'}}>
-          <List>
-            <ListItem disabled={true}
-              primaryText={(<p>Content {props.contract.address}</p>)}
-            />
-          </List>
+          <Table>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn >Address</TableHeaderColumn>
+                <TableHeaderColumn >Reward</TableHeaderColumn>
+                <TableHeaderColumn ></TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              <TableRow>
+                <TableRowColumn>
+                  1234
+                </TableRowColumn>
+                <TableRowColumn>
+                  1 ETH
+                </TableRowColumn>
+                <TableRowColumn>
+                  <RaisedButton label="Exploit" secondary={true} />
+                </TableRowColumn>
+              </TableRow>
+            </TableBody>
+          </Table>
         </Paper>
-        <Paper zDepth={1}>
-          <List>
-            <ListItem disabled={true}
-              primaryText={(<p>Some footer</p>)}
-            />
-          </List>
+        <Paper zDepth={1} style={{height:'200px', padding:'5px'}}>
+          <h2>Add new Bounty</h2>
+          <TextField
+            hintText="Target contract address"
+          />
+          <TextField
+            hintText="Bounty contract address"
+          />
+          <TextField
+            hintText="Reward in ETH"
+          />
+          <FloatingActionButton style={{position: 'absolute', right: '15px'}}>
+            <ContentAdd />
+          </FloatingActionButton>
         </Paper>
       </div>
     </div>
