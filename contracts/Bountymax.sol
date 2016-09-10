a95bc84a36b701144d8baabd06fc4fbdc4a69ee7
Andrew Jones <ascjones@gmail.com>
2016-09-10 17:57:21 +0100

    Implement register and dumb exploit (not calling oraclize yet

--- contracts/Bountymax.sol
+++ contracts/Bountymax.sol
@@ -1,28 +1,73 @@
 contract Bountymax {
   struct Bounty {
-      string name;
-      address target;
-      uint256 reward;
+    string name;
+    address target;
+    address invariant;
+    uint reward;
   }
-  mapping (address => Bounty) public bounties;
 
-  function register(address target, address invariant) public {
+  struct Exploit {
+    address target;
+    address hunter;
+  }
+
+  mapping (address => Bounty) public bounties;
+  mapping (address => Exploit) public exploits;
+
+  function register(string name, address target, address invariant) public {
+    // todo: avoid overwriting existing bounty?
+    bounties[target] = Bounty({name: name, target: target, invariant: invariant, reward: msg.value});
+    BountyRegistered(name, target, invariant, msg.value);
   }
 
   function exploit(address target, address bounty, address exploit) public {
+      // todo: check if bounty already claimed?
+      // for now just simulate that it succeeded
+
+      // so we can pay the hunter if it succeeds
+      exploits[exploit] = Exploit({target: target, hunter: msg.sender});
+
+      // call the callback directly for now
+      // todo: remove this call directly to callback with call to oraclize
+      __callback(msg.sender, "true");
   }
 
+  function __callback(address exploit, string result) {
+    // todo: add this back once we add in oraclise API contract
+    /*if (msg.sender != oraclize_cbAddress()) throw;*/
+    Exploit exp = exploits[exploit];
+    if (sha3(result) == sha3("true")) {
 
-  // todo: getBounties?
+      // use withdrawal pattern to avoid reentrancy bug
+      uint reward = bounties[exp.target].reward;
+      bounties[exp.target].reward = 0;
+      
+      if (exp.hunter.send(reward)) {
+        BountyClaimed({target: exp.target, hunter: exp.hunter, amount: reward});
+        // todo: remove exploit?
+      } else {
+        bounties[exp.target].reward = reward;
+      }
+    } else {
+      ExploitFailed({exploit: exploit, target: exp.target});
+    }
+  }
 
-  event ExploitTested (
-    bytes32 exploit,
-    bool success
+  event ExploitFailed (
+    address exploit,
+    address target
   );
 
   event BountyRegistered (
     string name,
     address target,
-    uint256 reward
+    address invariant,
+    uint reward
+  );
+
+  event BountyClaimed (
+    address target,
+    address hunter,
+    uint amount
   );
 }
