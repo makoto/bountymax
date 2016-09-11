6f22fd9a4684e1ee925384d26eb1c9c6917c0072
Makoto Inoue <inouemak@googlemail.com>
2016-09-11 12:58:40 +0100

    Rename to bountyIndex

--- app/javascripts/connector.js
+++ app/javascripts/connector.js
@@ -6,6 +6,7 @@ export default class Connector{
   constructor(web3, contract) {
     this.web3 = web3;
     this.contract = contract;
+    console.log('CONTRACT', contract)
     this.emitter = emitter;
     this.getAccount().then((account) =>{
       this.account = account;
@@ -33,19 +34,26 @@ export default class Connector{
   }
 
   getBounties(callback){
+    console.log('getBounties1')
     let contract = this.contract;
+    console.log('getBounties2', this.contract, contract)
     contract.numBounties.call().then(value => {
+      console.log('getBounties3')
       let bountiesArray = []
       for (var i = 1; i <= value.toNumber(); i++) {
         bountiesArray.push(i);
       }
+      console.log('getBounties4')
       Promise.all(bountiesArray.map(index => {
-        return contract.bountiesIndex.call(index).then(address => {
+        console.log('getBounties5')
+        return contract.bountyIndex.call(index).then(address => {
+          console.log('getBounties6')
           return contract.bounties.call(address);
         })
       })).then(function(bounties){
+        console.log('getBounties7')
         return bounties.map(bounty => {
-          console.log('getBounties')
+          console.log('getBounties8')
           var object =  {
             name: bounty[0],
             owner: bounty[1],
@@ -61,18 +69,18 @@ export default class Connector{
     })
   }
 
-  register({name, targetAddress, bountyAddress, deposit}){
+  register({name, targetAddress, invariantAddress, deposit}){
     this.contract.register.sendTransaction(
       name,
       targetAddress,
-      bountyAddress,
+      invariantAddress,
       {from:this.account, value:deposit}
     )
   }
 
-  exploit(targetAddress, exploitAddress){
+  exploit(targetAddress, invariantAddress, exploitAddress){
     this.contract.exploit.sendTransaction(
-      targetAddress, exploitAddress,{from:this.account}
+      targetAddress, invariantAddress, exploitAddress, {from:this.account}
     )
   }
 }
