14fb79c4fd5f26b20ca115899a54e58f55698243
Makoto Inoue <inouemak@googlemail.com>
2016-09-11 14:33:50 +0100

    Change title

--- app/javascripts/components/app.js
+++ app/javascripts/components/app.js
@@ -15,7 +15,7 @@ const App = (props) => (
     <div>
       <AppBar titleStyle={{textAlign:'center', fontSize:'xx-large', fontFamily:'sans-serif'}} style={{backgroundColor:"#46ae32"}}
         title={
-          <span>BountyMax<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - The Mad Max of Cyber Bounty  -</span></span>
+          <span>Bountymax<span style={{fontSize:'small', fontFamily:'sans-serif'}}> - The Mad Max of Cyber Bounty  -</span></span>
         }
         iconElementLeft={<Avatar src="https://15254b2dcaab7f5478ab-24461f391e20b7336331d5789078af53.ssl.cf1.rackcdn.com/ethereum.vanillaforums.com/favicon_85d47ba50743e3c3.ico" size={50} backgroundColor="white" />}
         iconElementRight={
